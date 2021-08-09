68ecea683db76c8faddf820f6930773773bf5a57
nick9lee <nicholas9lee@gmail.com>
2021-08-08 20:08:53 -0600

    added more buttons

--- SearchAlgoVisualizer/Controller/ViewController.swift
+++ SearchAlgoVisualizer/Controller/ViewController.swift
@@ -31,7 +31,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     var state07 = ButtonState()
     var state08 = ButtonState()
     var state09 = ButtonState()
-    var state010 = ButtonState()
+    var state0_10 = ButtonState()
     
     //row1
     var state10 = ButtonState()
@@ -44,7 +44,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     var state17 = ButtonState()
     var state18 = ButtonState()
     var state19 = ButtonState()
-    var state110 = ButtonState()
+    var state1_10 = ButtonState()
     
     //row2
     var state20 = ButtonState()
@@ -57,7 +57,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     var state27 = ButtonState()
     var state28 = ButtonState()
     var state29 = ButtonState()
-    var state210 = ButtonState()
+    var state2_10 = ButtonState()
     
     //row3
     var state30 = ButtonState()
@@ -70,7 +70,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     var state37 = ButtonState()
     var state38 = ButtonState()
     var state39 = ButtonState()
-    var state310 = ButtonState()
+    var state3_10 = ButtonState()
     
     //row4
     var state40 = ButtonState()
@@ -83,7 +83,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     var state47 = ButtonState()
     var state48 = ButtonState()
     var state49 = ButtonState()
-    var state410 = ButtonState()
+    var state4_10 = ButtonState()
     
     //row5
     var state50 = ButtonState()
@@ -96,7 +96,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     var state57 = ButtonState()
     var state58 = ButtonState()
     var state59 = ButtonState()
-    var state510 = ButtonState()
+    var state5_10 = ButtonState()
     
     //row6
     var state60 = ButtonState()
@@ -109,7 +109,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     var state67 = ButtonState()
     var state68 = ButtonState()
     var state69 = ButtonState()
-    var state610 = ButtonState()
+    var state6_10 = ButtonState()
     
     //row7
     var state70 = ButtonState()
@@ -122,7 +122,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     var state77 = ButtonState()
     var state78 = ButtonState()
     var state79 = ButtonState()
-    var state710 = ButtonState()
+    var state7_10 = ButtonState()
     
     //row8
     var state80 = ButtonState()
@@ -135,7 +135,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     var state87 = ButtonState()
     var state88 = ButtonState()
     var state89 = ButtonState()
-    var state810 = ButtonState()
+    var state8_10 = ButtonState()
     
     //row9
     var state90 = ButtonState()
@@ -148,7 +148,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     var state97 = ButtonState()
     var state98 = ButtonState()
     var state99 = ButtonState()
-    var state910 = ButtonState()
+    var state9_10 = ButtonState()
     
     //row10
     var state100 = ButtonState()
@@ -161,7 +161,72 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     var state107 = ButtonState()
     var state108 = ButtonState()
     var state109 = ButtonState()
-    var state1010 = ButtonState()
+    var state10_10 = ButtonState()
+    
+    //row11
+    var state110 = ButtonState()
+    var state111 = ButtonState()
+    var state112 = ButtonState()
+    var state113 = ButtonState()
+    var state114 = ButtonState()
+    var state115 = ButtonState()
+    var state116 = ButtonState()
+    var state117 = ButtonState()
+    var state118 = ButtonState()
+    var state119 = ButtonState()
+    var state11_10 = ButtonState()
+    
+    //row12
+    var state120 = ButtonState()
+    var state121 = ButtonState()
+    var state122 = ButtonState()
+    var state123 = ButtonState()
+    var state124 = ButtonState()
+    var state125 = ButtonState()
+    var state126 = ButtonState()
+    var state127 = ButtonState()
+    var state128 = ButtonState()
+    var state129 = ButtonState()
+    var state12_10 = ButtonState()
+    
+    //row13
+    var state130 = ButtonState()
+    var state131 = ButtonState()
+    var state132 = ButtonState()
+    var state133 = ButtonState()
+    var state134 = ButtonState()
+    var state135 = ButtonState()
+    var state136 = ButtonState()
+    var state137 = ButtonState()
+    var state138 = ButtonState()
+    var state139 = ButtonState()
+    var state13_10 = ButtonState()
+    
+    //row14
+    var state140 = ButtonState()
+    var state141 = ButtonState()
+    var state142 = ButtonState()
+    var state143 = ButtonState()
+    var state144 = ButtonState()
+    var state145 = ButtonState()
+    var state146 = ButtonState()
+    var state147 = ButtonState()
+    var state148 = ButtonState()
+    var state149 = ButtonState()
+    var state14_10 = ButtonState()
+    
+    //row15
+    var state150 = ButtonState()
+    var state151 = ButtonState()
+    var state152 = ButtonState()
+    var state153 = ButtonState()
+    var state154 = ButtonState()
+    var state155 = ButtonState()
+    var state156 = ButtonState()
+    var state157 = ButtonState()
+    var state158 = ButtonState()
+    var state159 = ButtonState()
+    var state15_10 = ButtonState()
     
     //button states end ------------------------------------------
 
@@ -183,7 +248,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     @IBOutlet weak var Button07: UIButton!
     @IBOutlet weak var Button08: UIButton!
     @IBOutlet weak var Button09: UIButton!
-    @IBOutlet weak var Button010: UIButton!
+    @IBOutlet weak var Button0_10: UIButton!
     
     //row1
     @IBOutlet weak var Button10: UIButton!
@@ -196,7 +261,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     @IBOutlet weak var Button17: UIButton!
     @IBOutlet weak var Button18: UIButton!
     @IBOutlet weak var Button19: UIButton!
-    @IBOutlet weak var Button110: UIButton!
+    @IBOutlet weak var Button1_10: UIButton!
     
     //row2
     @IBOutlet weak var Button20: UIButton!
@@ -209,7 +274,8 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     @IBOutlet weak var Button27: UIButton!
     @IBOutlet weak var Button28: UIButton!
     @IBOutlet weak var Button29: UIButton!
-    @IBOutlet weak var Button210: UIButton!
+    @IBOutlet weak var Button2_10: UIButton!
+    
     
     //row3
     @IBOutlet weak var Button30: UIButton!
@@ -222,7 +288,156 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     @IBOutlet weak var Button37: UIButton!
     @IBOutlet weak var Button38: UIButton!
     @IBOutlet weak var Button39: UIButton!
-    @IBOutlet weak var Button310: UIButton!
+    @IBOutlet weak var Button3_10: UIButton!
+    
+    //row4
+    @IBOutlet weak var Button40: UIButton!
+    @IBOutlet weak var Button41: UIButton!
+    @IBOutlet weak var Button42: UIButton!
+    @IBOutlet weak var Button43: UIButton!
+    @IBOutlet weak var Button44: UIButton!
+    @IBOutlet weak var Button45: UIButton!
+    @IBOutlet weak var Button46: UIButton!
+    @IBOutlet weak var Button47: UIButton!
+    @IBOutlet weak var Button48: UIButton!
+    @IBOutlet weak var Button49: UIButton!
+    @IBOutlet weak var Button4_10: UIButton!
+    
+    //row5
+    @IBOutlet weak var Button50: UIButton!
+    @IBOutlet weak var Button51: UIButton!
+    @IBOutlet weak var Button52: UIButton!
+    @IBOutlet weak var Button53: UIButton!
+    @IBOutlet weak var Button54: UIButton!
+    @IBOutlet weak var Button55: UIButton!
+    @IBOutlet weak var Button56: UIButton!
+    @IBOutlet weak var Button57: UIButton!
+    @IBOutlet weak var Button58: UIButton!
+    @IBOutlet weak var Button59: UIButton!
+    @IBOutlet weak var Button5_10: UIButton!
+    
+    //row6
+    @IBOutlet weak var Button60: UIButton!
+    @IBOutlet weak var Button61: UIButton!
+    @IBOutlet weak var Button62: UIButton!
+    @IBOutlet weak var Button63: UIButton!
+    @IBOutlet weak var Button64: UIButton!
+    @IBOutlet weak var Button65: UIButton!
+    @IBOutlet weak var Button66: UIButton!
+    @IBOutlet weak var Button67: UIButton!
+    @IBOutlet weak var Button68: UIButton!
+    @IBOutlet weak var Button69: UIButton!
+    @IBOutlet weak var Button6_10: UIButton!
+    
+    //row7
+    @IBOutlet weak var Button70: UIButton!
+    @IBOutlet weak var Button71: UIButton!
+    @IBOutlet weak var Button72: UIButton!
+    @IBOutlet weak var Button73: UIButton!
+    @IBOutlet weak var Button74: UIButton!
+    @IBOutlet weak var Button75: UIButton!
+    @IBOutlet weak var Button76: UIButton!
+    @IBOutlet weak var Button77: UIButton!
+    @IBOutlet weak var Button78: UIButton!
+    @IBOutlet weak var Button79: UIButton!
+    @IBOutlet weak var Button7_10: UIButton!
+    
+    //row8
+    @IBOutlet weak var Button80: UIButton!
+    @IBOutlet weak var Button81: UIButton!
+    @IBOutlet weak var Button82: UIButton!
+    @IBOutlet weak var Button83: UIButton!
+    @IBOutlet weak var Button84: UIButton!
+    @IBOutlet weak var Button85: UIButton!
+    @IBOutlet weak var Button86: UIButton!
+    @IBOutlet weak var Button87: UIButton!
+    @IBOutlet weak var Button88: UIButton!
+    @IBOutlet weak var Button89: UIButton!
+    @IBOutlet weak var Button8_10: UIButton!
+    
+    //row9
+    @IBOutlet weak var Button90: UIButton!
+    @IBOutlet weak var Button91: UIButton!
+    @IBOutlet weak var Button92: UIButton!
+    @IBOutlet weak var Button93: UIButton!
+    @IBOutlet weak var Button94: UIButton!
+    @IBOutlet weak var Button95: UIButton!
+    @IBOutlet weak var Button96: UIButton!
+    @IBOutlet weak var Button97: UIButton!
+    @IBOutlet weak var Button98: UIButton!
+    @IBOutlet weak var Button99: UIButton!
+    @IBOutlet weak var Button9_10: UIButton!
+    
+    //row10
+    @IBOutlet weak var Button100: UIButton!
+    @IBOutlet weak var Button101: UIButton!
+    @IBOutlet weak var Button102: UIButton!
+    @IBOutlet weak var Button103: UIButton!
+    @IBOutlet weak var Button104: UIButton!
+    @IBOutlet weak var Button105: UIButton!
+    @IBOutlet weak var Button106: UIButton!
+    @IBOutlet weak var Button107: UIButton!
+    @IBOutlet weak var Button108: UIButton!
+    @IBOutlet weak var Button109: UIButton!
+    @IBOutlet weak var Button10_10: UIButton!
+    
+    //row11
+    @IBOutlet weak var Button110: UIButton!
+    @IBOutlet weak var Button111: UIButton!
+    @IBOutlet weak var Button112: UIButton!
+    @IBOutlet weak var Button113: UIButton!
+    @IBOutlet weak var Button114: UIButton!
+    @IBOutlet weak var Button115: UIButton!
+    @IBOutlet weak var Button116: UIButton!
+    @IBOutlet weak var Button117: UIButton!
+    @IBOutlet weak var Button118: UIButton!
+    @IBOutlet weak var Button119: UIButton!
+    @IBOutlet weak var Button11_10: UIButton!
+    
+    //row12
+    @IBOutlet weak var Button120: UIButton!
+    @IBOutlet weak var Button121: UIButton!
+    @IBOutlet weak var Button122: UIButton!
+    @IBOutlet weak var Button123: UIButton!
+    @IBOutlet weak var Button124: UIButton!
+    @IBOutlet weak var Button125: UIButton!
+    @IBOutlet weak var Button126: UIButton!
+    @IBOutlet weak var Button127: UIButton!
+    @IBOutlet weak var Button128: UIButton!
+    @IBOutlet weak var Button129: UIButton!
+    @IBOutlet weak var Button12_10: UIButton!
+    
+    //row13
+    @IBOutlet weak var Button130: UIButton!
+    @IBOutlet weak var Button131: UIButton!
+    @IBOutlet weak var Button132: UIButton!
+    @IBOutlet weak var Button133: UIButton!
+    @IBOutlet weak var Button134: UIButton!
+    @IBOutlet weak var Button135: UIButton!
+    @IBOutlet weak var Button136: UIButton!
+    @IBOutlet weak var Button137: UIButton!
+    @IBOutlet weak var Button138: UIButton!
+    @IBOutlet weak var Button139: UIButton!
+    @IBOutlet weak var Button13_10: UIButton!
+    
+    //row14
+    @IBOutlet weak var Button140: UIButton!
+    @IBOutlet weak var Button141: UIButton!
+    @IBOutlet weak var Button142: UIButton!
+    @IBOutlet weak var Button143: UIButton!
+    @IBOutlet weak var Button144: UIButton!
+    @IBOutlet weak var Button145: UIButton!
+    @IBOutlet weak var Button146: UIButton!
+    @IBOutlet weak var Button147: UIButton!
+    @IBOutlet weak var Button148: UIButton!
+    @IBOutlet weak var Button149: UIButton!
+    @IBOutlet weak var Button14_10: UIButton!
+    
+    //row15
+    
+    
+    
+    
     
     
     //cell buttons end------------------------------
@@ -260,7 +475,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         state07.resetState()
         state08.resetState()
         state09.resetState()
-        state010.resetState()
+        state0_10.resetState()
         
         //row1
         state10.resetState()
@@ -286,7 +501,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         state27.resetState()
         state28.resetState()
         state29.resetState()
-        state210.resetState()
+        state2_10.resetState()
         
         //row3
         state30.resetState()
@@ -299,7 +514,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         state37.resetState()
         state38.resetState()
         state39.resetState()
-        state310.resetState()
+        state3_10.resetState()
         
         //reseting all cell colors ----------------
         //row0
@@ -313,7 +528,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         Button07.backgroundColor = UIColor(named: "defaultTileColor")
         Button08.backgroundColor = UIColor(named: "defaultTileColor")
         Button09.backgroundColor = UIColor(named: "defaultTileColor")
-        Button010.backgroundColor = UIColor(named: "defaultTileColor")
+        Button0_10.backgroundColor = UIColor(named: "defaultTileColor")
         
         //row1
         Button10.backgroundColor = UIColor(named: "defaultTileColor")
@@ -326,7 +541,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         Button17.backgroundColor = UIColor(named: "defaultTileColor")
         Button18.backgroundColor = UIColor(named: "defaultTileColor")
         Button19.backgroundColor = UIColor(named: "defaultTileColor")
-        Button110.backgroundColor = UIColor(named: "defaultTileColor")
+        Button1_10.backgroundColor = UIColor(named: "defaultTileColor")
         
         //row2
         Button20.backgroundColor = UIColor(named: "defaultTileColor")
@@ -339,7 +554,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         Button27.backgroundColor = UIColor(named: "defaultTileColor")
         Button28.backgroundColor = UIColor(named: "defaultTileColor")
         Button29.backgroundColor = UIColor(named: "defaultTileColor")
-        Button210.backgroundColor = UIColor(named: "defaultTileColor")
+        Button2_10.backgroundColor = UIColor(named: "defaultTileColor")
         
         //row3
         Button30.backgroundColor = UIColor(named: "defaultTileColor")
@@ -352,7 +567,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         Button37.backgroundColor = UIColor(named: "defaultTileColor")
         Button38.backgroundColor = UIColor(named: "defaultTileColor")
         Button39.backgroundColor = UIColor(named: "defaultTileColor")
-        Button310.backgroundColor = UIColor(named: "defaultTileColor")
+        Button3_10.backgroundColor = UIColor(named: "defaultTileColor")
         
     }
     @IBAction func runPressed(_ sender: Any) {
@@ -411,8 +626,8 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
             button = Button09
             break
         case(0,10):
-            state = state010
-            button = Button010
+            state = state0_10
+            button = Button0_10
             break
         case(1,0):                  //row 1 --------
             state = state10
@@ -455,8 +670,8 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
             button = Button19
             break
         case(1,10):
-            state = state110
-            button = Button110
+            state = state1_10
+            button = Button1_10
             break
         case (_, _):
             return
