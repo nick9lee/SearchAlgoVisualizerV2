c9192d58e13dcaf3e8af365016827c496992893a
nick9lee <nicholas9lee@gmail.com>
2021-09-01 12:40:53 -0600

    added

--- SearchAlgoVisualizer/Model/MinHeap.swift
+++ SearchAlgoVisualizer/Model/MinHeap.swift
@@ -8,5 +8,41 @@
 import Foundation
 
 class MinHeap {
+    var heap: [Int]
+    var nodePositionsInHeap: [String: Int]
     
+    init(array: [Node]) {
+        heap = []
+        nodePositionsInHeap = [:]
+        
+        var i = 0
+        for x in array {
+            nodePositionsInHeap[x.id] = i
+            i += 1
+        }
+        //heap = buildHeap(array);
+    }
+    
+    public func buildHeap(array: [Node]) -> [Node] {
+        let firstParentIdx: Int = (array.count - 2) / 2
+        
+        var currentIdx = firstParentIdx + 1
+        
+        while currentIdx >= 0 {
+            // array = siftDown(curretnIdx, array.count - 1, array)
+            currentIdx -= 1
+        }
+        
+        return array
+    }
+    
+    public func isEmpty() -> Bool{
+        return heap.isEmpty
+    }
+    
+    public func siftDown(currentIdx: Int, endIdx: Int, array: [Node]) -> [Node]{
+        
+        
+        return array
+    }
 }
--- SearchAlgoVisualizer/Model/searchAlgos.swift
+++
@@ -1,8 +0,0 @@
-//
-//  searchAlgos.swift
-//  SearchAlgoVisualizer
-//
-//  Created by Nicholas Lee on 2021-07-13.
-//
-
-import Foundation
