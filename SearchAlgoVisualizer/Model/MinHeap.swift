f4331267cb20781fbf6e6b6055ffc72cf6708204
nick9lee <nicholas9lee@gmail.com>
2021-09-05 23:14:20 -0600

    added min heap

--- SearchAlgoVisualizer/Model/MinHeap.swift
+++ SearchAlgoVisualizer/Model/MinHeap.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 class MinHeap {
-    var heap: [Int]
+    var heap: [Node]
     var nodePositionsInHeap: [String: Int]
     
     init(array: [Node]) {
@@ -20,16 +20,18 @@ class MinHeap {
             nodePositionsInHeap[x.id] = i
             i += 1
         }
-        //heap = buildHeap(array);
+
+        heap = buildHeap(arraytemp: array)
     }
     
-    public func buildHeap(array: [Node]) -> [Node] {
+    public func buildHeap(arraytemp: [Node]) -> [Node] {
+        var array = arraytemp
         let firstParentIdx: Int = (array.count - 2) / 2
         
         var currentIdx = firstParentIdx + 1
         
         while currentIdx >= 0 {
-            // array = siftDown(curretnIdx, array.count - 1, array)
+            array = siftDown(currentIdxTemp: currentIdx, endIdx: array.count - 1, array: array)
             currentIdx -= 1
         }
         
@@ -40,9 +42,92 @@ class MinHeap {
         return heap.isEmpty
     }
     
-    public func siftDown(currentIdx: Int, endIdx: Int, array: [Node]) -> [Node]{
+    public func siftDown(currentIdxTemp: Int, endIdx: Int, array: [Node]) -> [Node]{
         
+        var currentIdx = currentIdxTemp
+        var childOneIdx = currentIdx * 2 + 1;
+        var childTwoIdx: Int
         
+        while childOneIdx <= endIdx {
+            childTwoIdx = currentIdx * 2 + 2 <= endIdx ? currentIdx * 2 + 2 : -1
+            
+            var idxToSwap: Int
+            
+            if childTwoIdx != -1 && (array[childTwoIdx].estimatedDistanceToEnd < heap[childOneIdx].estimatedDistanceToEnd) {
+                idxToSwap = childTwoIdx
+            } else {
+                idxToSwap = childOneIdx
+            }
+            
+            if array[idxToSwap].estimatedDistanceToEnd < array[currentIdx].estimatedDistanceToEnd {
+                
+                //swap(currentIdx, idxToSwap);
+                nodePositionsInHeap[heap[currentIdx].id] = idxToSwap
+                nodePositionsInHeap[heap[idxToSwap].id] = currentIdx
+                let temp = heap[currentIdx]
+                heap[currentIdx] = heap[idxToSwap]
+                heap[idxToSwap] = temp
+                
+                currentIdx = idxToSwap
+                childOneIdx = currentIdx * 2 + 1
+            } else {
+                return array
+            }
+        }
         return array
+    }
+    
+    public func siftUp(currentIdxTemp: Int){
+        var currentIdx = currentIdxTemp
+        var parentIdx: Int = (currentIdx - 1) / 2
+        
+        while currentIdx > 0 && heap[currentIdx].estimatedDistanceToEnd < heap[parentIdx].estimatedDistanceToEnd {
+            
+            //swap (currentIdx, parent Idx)
+            nodePositionsInHeap[heap[currentIdx].id] = parentIdx
+            nodePositionsInHeap[heap[parentIdx].id] = currentIdx
+            let temp = heap[currentIdx]
+            heap[currentIdx] = heap[parentIdx]
+            heap[parentIdx] = temp
+            
+            currentIdx = parentIdx
+            parentIdx = (currentIdx - 1) / 2
+        }
+    }
+    
+    public func remove() -> Node?{
+        if isEmpty() {
+            return nil
+        }
+        
+        //swap(0, heap.size() - 1)
+        nodePositionsInHeap[heap[0].id] = heap.count - 1
+        nodePositionsInHeap[heap[heap.count - 1].id] = 0
+        let temp = heap[0]
+        heap[0] = heap[heap.count - 1]
+        heap[heap.count - 1] = temp
+        
+        let node = heap[heap.count - 1]
+        heap.remove(at: heap.count - 1)
+        nodePositionsInHeap.removeValue(forKey: node.id)
+        heap = siftDown(currentIdxTemp: 0, endIdx: heap.count - 1, array: heap)
+        
+        return node
+    }
+    
+    public func insert(node: Node) {
+        heap.append(node)
+        nodePositionsInHeap[node.id] = heap.count - 1
+        siftUp(currentIdxTemp: heap.count - 1)
+    }
+    
+    public func containsNode(node: Node) -> Bool {
+        return nodePositionsInHeap[node.id] != nil
+    }
+    
+    public func update(node: Node){
+        if nodePositionsInHeap[node.id] != nil {
+            siftUp(currentIdxTemp: nodePositionsInHeap[node.id]!)
+        }
     }
 }
